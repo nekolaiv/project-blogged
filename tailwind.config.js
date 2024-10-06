ed263ac74b231d8a4f7fc647924370aea61512ee
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-06 14:41:37 +0800

    embed tailwind and flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./public/**/*.{html,js,php}", 
-    "./src/**/*.{html,js,php}", 
+    "./src/**/*.{html,js,php}",
+    "./node_modules/flowbite/**/*.js" 
   ], 
   theme: {
     extend: {},
