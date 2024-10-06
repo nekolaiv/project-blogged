5758b24418245040712afae1beb433b06b3d5e45
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-06 14:41:58 +0800

    restructure

--- src/views/landing/about.php
+++
@@ -1,5 +0,0 @@
-<?php require "../../includes/header.php" ?>
-<main>
-    <h1>About</h1>
-</main>
-<?php require "../../includes/footer.php" ?>
--- src/views/landing/home.php
+++
@@ -1,5 +0,0 @@
-<?php require "../../includes/header.php" ?>
-<main>
-    <h1>Main</h1>
-</main>
-<?php require "../../includes/footer.php" ?>
